04ca005705122027cf586850e14127f0b7a74d8e
Lee Jones <scribblethink@gmail.com>
2010-03-02 08:18:30 -0500

    Added option for resque web interface deployment and fixed redis conf file

--- lib/resque.rb
+++ lib/resque.rb
@@ -3,7 +3,7 @@ module Resque
   def redis(options = {})
     package 'redis-server', :ensure => 'absent'
     gem 'redis'
-    gem 'resque' # needed for the rake test in the install step
+    # gem 'resque' # needed for the rake test in the install step
 
     exec 'install redis',
       :cwd => '/tmp',
@@ -30,11 +30,37 @@ module Resque
       :require => file('/etc/redis.conf')
   end
 
-  def resque(options = {} )
-    gem 'resque'
+  def resque_web(options = {} )
+    %w(json thin sinatra).each do |g|
+      gem g, :ensure => :installed
+    end
+    
+    file "#{configuration[:deploy_to]}/shared/resque_web", :ensure => :directory, :before => file("resque_web_rack")
+    file "#{configuration[:deploy_to]}/shared/resque_web/public", :ensure => :directory, :before => file("resque_web_rack")
+    file "#{configuration[:deploy_to]}/shared/resque_web/tmp", :ensure => :directory, :before => file("resque_web_rack")
 
-    exec 'install resque',
-      :require => service('redis-server')
+    file "#{configuration[:deploy_to]}/shared/resque_web/config.ru",
+      :content => template(File.join(File.dirname(__FILE__), '..', 'templates', 'config.ru.erb'), binding),
+      :ensure => :file,
+      :mode => '644',
+      :require => exec('install redis'),
+      :notify => service('apache2'),
+      :alias => "resque_web_rack"
+    
+    
+    file '/etc/apache2/sites-available/resque_web',
+      :content => template(File.join(File.dirname(__FILE__), '..', 'templates', 'resque_web.vhost.erb'), binding),
+      :ensure => :file,
+      :mode => '644',
+      :require => exec('install redis'),
+      :notify => service('apache2'),
+      :alias => "resque_web_vhost"
+      
+    a2ensite "resque_web", :require => file("resque_web_vhost")
+  end
+  
+  def resque(options = {})
+    gem "resque", :ensure => :installed
   end
   
 end
