b16482d1d7ee9b95cff840b3c444a9422e03040b
Lee Jones <scribblethink@gmail.com>
2010-03-02 10:54:25 -0500

    Removed Redis configuration management and replaced with dependency on moonshine_redis

--- lib/resque.rb
+++ lib/resque.rb
@@ -1,35 +1,9 @@
 module Resque
 
-  def redis(options = {})
-    package 'redis-server', :ensure => 'absent'
-    gem 'redis'
-    # gem 'resque' # needed for the rake test in the install step
-
-    exec 'install redis',
-      :cwd => '/tmp',
-      :command => [
-        'git clone git://github.com/defunkt/resque.git',
-        'cd /tmp/resque',
-        'rake redis:install dtach:install',
-      ].join(' && '),
-      :creates => '/usr/bin/redis-server'
-
-    file '/etc/redis.conf',
-      :content => template(File.join(File.dirname(__FILE__), '..', 'templates', 'redis.conf.erb'), binding),
-      :ensure => :file,
-      :mode => '644',
-      :require => exec('install redis'),
-      :notify => service('redis-server')
-
-    service 'redis-server',
-      :provider => :base,
-      :start => '/usr/bin/redis-server /etc/redis.conf',
-      :pattern => 'redis-server',
-      :ensure => :running,
-      :enable => true,
-      :require => file('/etc/redis.conf')
+  def resque(options = {})
+    gem "resque", :ensure => :installed
   end
-
+  
   def resque_web(options = {} )
     %w(json thin sinatra).each do |g|
       gem g, :ensure => :installed
@@ -43,24 +17,19 @@ module Resque
       :content => template(File.join(File.dirname(__FILE__), '..', 'templates', 'config.ru.erb'), binding),
       :ensure => :file,
       :mode => '644',
-      :require => exec('install redis'),
+      :require => service('redis-server'),
       :notify => service('apache2'),
       :alias => "resque_web_rack"
-    
     
     file '/etc/apache2/sites-available/resque_web',
       :content => template(File.join(File.dirname(__FILE__), '..', 'templates', 'resque_web.vhost.erb'), binding),
       :ensure => :file,
       :mode => '644',
-      :require => exec('install redis'),
+      :require => service('redis-server'),
       :notify => service('apache2'),
       :alias => "resque_web_vhost"
       
     a2ensite "resque_web", :require => file("resque_web_vhost")
   end
-  
-  def resque(options = {})
-    gem "resque", :ensure => :installed
-  end
-  
+    
 end
