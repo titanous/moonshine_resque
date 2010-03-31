bd1b13c8c5bfa1ac6fdc7062e383813771e56849
Ches Martin <ches@whiskeyandgrits.net>
2010-03-31 17:27:17 +0700

    Allow resque gem version to be configured.
    Resque defines gem deps for the redis and redis-namespace gems, so I'm
    thinking it's best to let rubygems handle them with what it's told.

--- spec/resque_spec.rb
+++ spec/resque_spec.rb
@@ -1,24 +1,34 @@
 require File.join(File.dirname(__FILE__), 'spec_helper.rb')
 
-class ResqueManifest < Moonshine::Manifest
+class ResqueManifest < Moonshine::Manifest::Rails
   plugin :resque
 end
 
 describe "A manifest with the Resque plugin" do
-  
+
   before do
     @manifest = ResqueManifest.new
-    @manifest.resque
   end
-  
+
   it "should be executable" do
     @manifest.should be_executable
   end
-  
-  #it "should provide packages/services/files" do
-  # @manifest.packages.keys.should include 'foo'
-  # @manifest.files['/etc/foo.conf'].content.should match /foo=true/
-  # @manifest.execs['newaliases'].refreshonly.should be_true
-  #end
-  
+
+  describe "using the `resque` recipe" do
+    before do
+      @manifest.resque
+    end
+
+    it "should install the resque and yajl-ruby packages" do
+      @manifest.packages.keys.should include('resque')
+      @manifest.packages.keys.should include('yajl-ruby')
+    end
+
+    it "should install specified resque gem version or default to latest" do
+      @manifest.packages['resque'].ensure.should == :latest
+      @manifest.resque(:version => '1.6.1')
+      @manifest.packages['resque'].ensure.should == '1.6.1'
+    end
+  end
+
 end
