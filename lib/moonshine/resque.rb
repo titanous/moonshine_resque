25233e6bc1b172d63b84b11d0d4481b64dab4241
Jonathan Rudenberg <jonathan@titanous.com>
2010-10-15 09:04:32 -0500

    Nginx support

--- lib/moonshine/resque.rb
+++ lib/moonshine/resque.rb
@@ -7,11 +7,9 @@ module Moonshine
       gem 'yajl-ruby', :ensure => :installed
     end
 
-    def resque_web(options = {})
-      %w(thin sinatra).each do |g|
-        gem g, :ensure => :installed
-      end
-      
+    def resque_web_shared
+      gem 'sinatra', :ensure => :installed
+
       directories = [
         "#{configuration[:deploy_to]}/shared/resque_web",  
         "#{configuration[:deploy_to]}/shared/resque_web/public",
@@ -26,6 +24,13 @@ module Moonshine
         :mode => '775'
       end
 
+    end
+
+    def resque_web_apache(options = {})
+      resque_web_shared
+
+      gem 'thin', :ensure => :installed
+
       file "#{configuration[:deploy_to]}/shared/resque_web/config.ru",
         :content => template(File.join(File.dirname(__FILE__), '..', '..', 'templates', 'config.ru.erb'), binding),
         :ensure => :file,
@@ -36,13 +41,38 @@ module Moonshine
         :alias => "resque_web_rack"
 
       file '/etc/apache2/sites-available/resque_web',
-        :content => template(File.join(File.dirname(__FILE__), '..', '..', 'templates', 'resque_web.vhost.erb'), binding),
+        :content => template(File.join(File.dirname(__FILE__), '..', '..', 'templates', 'resque_web.apache_vhost.erb'), binding),
         :ensure => :file,
         :mode => '644',
         :notify => service('apache2'),
         :alias => "resque_web_vhost"
 
       a2ensite "resque_web", :require => file("resque_web_vhost")
+    end
+
+    def resque_web_nginx
+      resque_web_shared
+
+      file "#{configuration[:deploy_to]}/shared/resque_web/config.ru",
+        :content => template(File.join(File.dirname(__FILE__), '..', '..', 'templates', 'config.ru.erb'), binding),
+        :ensure => :file,
+        :mode => '644',
+        :owner => configuration[:user],
+        :group => configuration[:group] || configuration[:user],
+        :notify => exec('nginx_reload'),
+        :alias => 'resque_web_rack'
+
+      file '/opt/nginx/conf/vhosts/resque_web.conf',
+        :content => template(File.join(File.dirname(__FILE__), '..', '..', 'templates', 'resque_web.nginx_vhost.erb'), binding),
+        :ensure => :file,
+        :mode => '644',
+        :notify => exec('nginx_reload'),
+        :alias => 'resque_web_vhost_conf'
+
+      file "/opt/nginx/conf/vhosts/on/resque_web.conf",
+        :ensure => "/opt/nginx/conf/vhosts/resque_web.conf",
+        :notify => [exec('nginx_reload')],
+        :alias => 'resque_web_vhost'
     end
 
   end
